f205a8edb38e730370e5dcd722463f5ddddadcd3
NateFang <natefang32@gmail.com>
2015-11-04 16:16:27 +0800

    add share scheme support

---
+++ JGActionSheet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JGActionSheet
+//
+//  Created by NateFang on 11/4/15.
+//  Copyright © 2015 NateFang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
